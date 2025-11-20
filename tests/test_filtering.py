f0271afdbc7ddf81e4eea93dcd0cb07a934f1030
evilsobol <evilsobol01@gmail.com>
2025-11-20 13:22:06 +0700

    test: добавление тестов для фильтрации отрицательных значений
    - Добавлен test_remove_negative для проверки отсечения отрицательных значений
    - Добавлен test_remove_negative_2 с матрицей 2x3 для дополнительной проверки
    - Оба теста проверяют работу clamp_zero=True с отключенной медианной фильтрацией
    - Убеждаются, что минимальное значение после фильтрации >= 0

--- tests/test_filtering.py
+++ tests/test_filtering.py
@@ -6,3 +6,9 @@ def test_remove_negative():
     s2 = apply_filters(s, median_size=None, clamp_zero=True)
     print(s2)
     assert s2.min() >= 0
+
+def test_remove_negative_2():
+    s = np.array([[1, -5, 7], [-1, 3, 6]])
+    s2 = apply_filters(s, median_size=None, clamp_zero=True)
+    print(s2)
+    assert s2.min() >= 0
